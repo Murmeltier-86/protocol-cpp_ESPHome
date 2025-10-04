95b7672d9748578d5989ffdd8aadc68c58cc4afb
Murmeltier-86 <fmayer1986@web.de>
2025-10-04 12:01:15 +0200

    Erlaube XML-Pfad ohne Zusatz-Includes

---
+++ esphome/components/jutta_proto/jura_mapping_embed.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstddef>
+
+namespace esphome {
+namespace jutta_component {
+
+const char *get_joe_xml_data();
+std::size_t get_joe_xml_length();
+const char *get_joe_xml_include_path();
+
+}  // namespace jutta_component
+}  // namespace esphome
+
